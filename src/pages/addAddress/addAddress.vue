895f4f7e6551e8586e54401093397eb260804f34
MChen <948849732@qq.com>
2018-01-22 18:10:45 +0800

    0122

--- src/pages/addAddress/addAddress.vue
+++ src/pages/addAddress/addAddress.vue
@@ -1,18 +1,166 @@
 <template>
     <div class="addAddress-page">
         <goback title='新建地址'></goback>
-        <areaPicker></areaPicker>
+        <section class='inputArea'>
+            <input type="text" placeholder="省份、城市、区县" @click="showAreaSelect" v-model='area'>
+            <input type="text" placeholder="详细地址，如街道、楼牌号等" v-model="fullAddress">
+            <input type="text" placeholder="姓名" v-model="name">
+            <input type="text" placeholder="手机号码" v-model="phone">
+        </section>
+        <section class='setDefault' @click="setDefault">
+            <span class='icon-select' :class="{select_true:areaDefault}"></span> <p>设为默认地址 </p> 
+        </section>
+        <section class="btn-save">
+            <button @click="save">保存</button>
+        </section>
+        <section class="area-picker" v-show="pickerVisible">
+            <van-area :area-list='areaList' 
+                @confirm='areaPickerConfirm'
+                @cancel='areaPickerCancel'>
+            </van-area>
+        </section>
     </div>
 </template>
 <script>
 import goback from '../../components/goback'
 import areaPicker from '../../components/areaPicker'
+import areaList from '../../config/area.json'
 export default{
+    data(){
+        return{
+            area:null,
+            fullAddress:null,
+            name:null,
+            phone:null,
+            areaList:null,
+            pickerVisible:false,
+            areaPickerValue:null,
+            areaDefault:false
+        }
+    },
     components:{
         goback,areaPicker
+    },
+    mounted(){
+        this.areaList = areaList;
+    },
+    methods:{
+        showAreaSelect(){
+            this.pickerVisible = true
+        },
+        areaPickerConfirm(value){
+            console.log(value);
+            this.pickerVisible = false;
+            let province = value[0].name,
+                city = value[1].name,
+                county = value[2].name;
+            this.area = province+' '+city+' '+county;
+        },
+        areaPickerCancel(){
+            this.pickerVisible = false;
+        },
+        setDefault(){
+            this.areaDefault = !this.areaDefault;
+        },
+        save(){
+            if(!this.area){
+                alert('请选择地区')
+            }else if(!this.fullAddress){
+                alert('请填写详细地址')
+            }else if(!this.name){
+                alert('请填写收货人姓名')
+            }else if(!this.phone){
+                alert('请输入手机号')
+            }else{
+                if( !(/1[34578]\d{9}$/.test(this.phone)) ){
+                    alert('请输入正确的手机号')                
+                }else{
+                    console.log(this.area,this.fullAddress,this.name,this.phone);
+                }
+            }
+        }
     }
 }
 </script>
 <style lang='scss'>
-
+.addAddress-page{
+    padding-top: 88px;
+    padding-bottom: 48px;
+    width: 100%;
+    min-height: 100%;
+    background-color: #fff;
+    position: relative;
+    .inputArea{
+        width: 100%;
+        input{
+            width: 100%;
+            height: 100px;
+            line-height: 100px;
+            font-size: 28px;
+            color:rgb(34, 34, 34);
+            border-bottom: 1px solid rgb(242, 242, 242);
+            padding-left: 46px;
+            padding-right: 46px;
+        }
+    }
+    .setDefault{
+        position: relative;
+        width: 220px;
+        margin: 30px auto;
+        p{
+            font-size: 26px;
+            color:rgb(112, 112, 112);
+            margin-left: 46px;
+        }
+        .icon-select{
+            position: absolute;
+            display: inline-block;
+            width: 30px;
+            height: 30px;
+            top: 5px;
+            background-image: url('../../img/icon/select_false.png');
+            background-size: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+        .select_true{
+            background-image: url('../../img/icon/select_true.png');
+        }
+    }
+    .btn-save{
+        width: 640px;
+        margin: 100px auto;
+        margin-bottom: 0;
+        button{
+            width: 100%;
+            height: 80px;
+            background-color: #fff;
+            border: 1px solid rgb(73, 144, 238);
+            color: rgb(73, 144, 238);
+            font-size: 28px;
+        }
+    }
+    .area-picker{
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 999;
+        background-color: rgba(112, 112, 112, 0.5);
+        .van-picker{
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            .van-hairline--top-bottom{
+                height: 80px;
+                line-height: 80px;
+                padding-left: 46px;
+                padding-right: 46px;
+                font-size: 28px;
+            }
+        }
+    }
+}
 </style>
--- src/pages/address/address.vue
+++ src/pages/address/address.vue
@@ -2,29 +2,36 @@
     <div class="address-page">
         <goback title='地址管理'></goback>
         <div class="container">
-            <section class="address-item" v-for="(item,index) in addresses">
-                <div class="info">
-                    <div class='row1'>
-                        <p class='name'>{{ item.name }}</p>
-                        <p class='phone'>{{item.phone}}</p>
+            <section v-if="address_list">
+                <section class="address-item" v-for="(item,index) in address_list">
+                    <div class="info">
+                        <div class='row1'>
+                            <p class='name'>{{ item.name }}</p>
+                            <p class='phone'>{{item.phone}}</p>
+                        </div>
+                        <div class='row2'>
+                            <button class='default' 
+                                :class="{active:item._default == '1'}"
+                                @click="selectDefault(index)">
+                                默认
+                            </button>
+                            <p class="address">{{ item.address }}</p>
+                        </div>
                     </div>
-                    <div class='row2'>
-                        <button class='default' 
-                            :class="{active:item._default == '1'}"
-                            @click="selectDefault(index)">
-                            默认
-                        </button>
-                        <p class="address">{{ item.address }}</p>
+                    <div class="edit">
+                        <img src="../../img/icon/edit@3x.png" alt="">
                     </div>
-                </div>
-                <div class="edit">
-                    <img src="../../img/icon/edit@3x.png" alt="">
-                </div>
+                </section>
+            </section>
+            <section class="address_list-is-empty" v-else>
+                <img src="../../img/address/address@3x.png" alt="">
+                <p>收货地址在哪里</p>
             </section>
             <section class="addAddress">
                 <button>+新建地址</button>
             </section>
         </div>
+        
     </div>
 </template>
 <script>
@@ -32,19 +39,20 @@ import goback from '../../components/goback'
 export default{
     data(){
         return{
-            addresses:[
-                {
-                    name:'小明',
-                    phone: '188****5678',
-                    address:'浙江省杭州市西湖区文二西路益乐新村',
-                    _default:"1"
-                },{
-                    name:'大头娃娃',
-                    phone: '188****5678',
-                    address:'河北省秦皇岛市海港区文化路368号国家税务局',
-                    _default:"0"
-                }
-            ]
+            // address_list:[
+            //     {
+            //         name:'小明',
+            //         phone: '188****5678',
+            //         address:'浙江省杭州市西湖区文二西路益乐新村',
+            //         _default:"1"
+            //     },{
+            //         name:'大头娃娃',
+            //         phone: '188****5678',
+            //         address:'河北省秦皇岛市海港区文化路368号国家税务局',
+            //         _default:"0"
+            //     }
+            // ]
+            address_list:null
         }
     },
     components:{
@@ -52,7 +60,7 @@ export default{
     },
     methods:{
         selectDefault(index){
-            let arr = this.addresses;
+            let arr = this.address_list;
             for (let i = 0; i < arr.length; i++) {
                 arr[i]._default = "0"
             }
@@ -63,12 +71,17 @@ export default{
 </script>
 <style lang='scss'>
 .address-page{
-    padding-top: 90px;
     height: 100%;
     background-color: #fff;
     position: relative;
     .container{
+        padding-top: 90px;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-wrap: wrap;
         .address-item{
+            width: 100%;
             padding: 20px 46px;
             border-bottom: 2px solid rgb(240, 240, 240);
             display: flex;
@@ -121,6 +134,22 @@ export default{
                     width: 38px;
                     height: 38px;
                 }
+            }
+        }
+        .address_list-is-empty{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            img{
+                width: 210px;
+                margin-bottom: 30px;
+            }
+            p{
+                font-size: 30px;
+                color:rgb(170, 170, 170);
             }
         }
     }
